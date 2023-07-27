7b16e62c46a4c1bd982e61c6d455388beba07c6c
Aman verma <9.amanverma1@gmail.com>
2023-07-27 19:49:55 +0530

    profileside page done

---
+++ src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import FollowersCard from '../FollowersCard/FollowersCard';
+import LogoSearch from '../LogoSearch/LogoSearch';
+import ProfileCard from '../ProfileCard/ProfileCard';
+
+import './ProfileSide.css';
+
+const ProfileSide = () => {
+  const [showProfile, setShowProfile] = useState(false);
+
+  const handleToggleProfile = () => {
+    setShowProfile((prevShowProfile) => !prevShowProfile);
+  };
+
+  return (
+    <div className="ProfileSide">
+
+      {/* <div className="ProfileSide"> */}
+
+      <LogoSearch />
+
+      <button className="toggleProfileBtn" onClick={handleToggleProfile}>
+        Your Profile
+      </button>
+
+
+      {showProfile && (
+        <div className="profile-container-mobile">
+          <ProfileCard />
+          <FollowersCard />
+        </div>
+      )}
+
+      {/* </div>  */}
+
+    </div>
+  )
+}
+
+export default ProfileSide
