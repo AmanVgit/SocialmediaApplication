832cbee660da8582b67e1a4b146bb8b857c5e902
Aman verma <9.amanverma1@gmail.com>
2023-08-03 02:18:21 +0530

    Post functionality added

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import connectDB from './mongodb/connect.js';
 
 import AuthRoute from './Routes/AuthRoute.js'
 import UserRoute from './Routes/UserRoute.js'
-// import PostRoute from './Routes/PostRoute.js'
+import PostRoute from './Routes/PostRoute.js'
 
 
 dotenv.config();
@@ -35,4 +35,4 @@ startServer();
 // usage of routes
 app.use('/auth', AuthRoute)
 app.use('/user', UserRoute)
-// app.use('/post', PostRoute)
+app.use('/post', PostRoute)
