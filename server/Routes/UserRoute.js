a853b4de55d929159837d80728617a07dc8dc8d4
Aman verma <9.amanverma1@gmail.com>
2023-08-03 01:44:05 +0530

    user delete,follow,unfollow route complete

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
-import {getUser,updateUser } from "../Controllers/UserController.js";
+import {getUser, updateUser, deleteUser, followUser, UnFollowUser} from "../Controllers/UserController.js";
 
 const router = express.Router();
 
 router.get('/:id', getUser)
 router.put('/:id', updateUser)
-// router.delete('/:id', deleteUser)
-// router.put('/:id/follow', followUser)
-// router.put('/:id/unfollow', UnFollowUser)
+router.delete('/:id', deleteUser)
+router.put('/:id/follow', followUser)
+router.put('/:id/unfollow', UnFollowUser)
 
 
 export default router;
