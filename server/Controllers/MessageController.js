9a647ee62b5e03033351f53467e0a9641f1a363b
Aman verma <88731279+AmanVgit@users.noreply.github.com>
2023-08-07 04:05:02 +0530

    Update MessageController.js

--- server/Controllers/MessageController.js
+++ server/Controllers/MessageController.js
@@ -1,4 +1,4 @@
-import MessageModel from "../models/MessageModel";
+import MessageModel from "../Models/MessageModel.js";
 
 export const addMessage = async (req, res) => {
   const { chatId, senderId, text } = req.body;
